7ab34406c6947926a0072e61f132772b54c5285a
kushamit67 <100716318+kushamit67@users.noreply.github.com>
2024-05-27 16:57:37 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,26 @@
+"use strict"
+
+//let score = "null"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// 33 = 33
+// true = 1
+// false = 0
+// 33abc = NaN
+// undefined & null = NaN, 
+
+//let str1 = 3
+//let str2 = 5
+
+//let str3 = str1 + str2
+//console.log(str3)
+console.log(1+2)
+console.log("1"+2)
+console.log("1"+2+3)
+console.log((((2+2)*3)%4))
