aed40c7b5da1ca783e81e921c2edcf9fd6b6283c
kushamit67 <creativeimaginator01@gmail.com>
2024-05-27 16:55:14 +0530

    initial commit on may 27

---
+++ index.js
@@ -0,0 +1,16 @@
+" user strict"; // use this code as a newer version of javascript
+/*console.log("Script started"); // Ensure the script is running
+
+let accountId = "amit"
+let accountCity = "Delhi"
+let accountEmail ="amit@test.com"
+let accountstatus= null;
+
+console.log("Before logging accountId"); // Checkpoint before logging accountId
+console.log(accountId)
+console.table([accountId, accountCity, accountEmail, accountstatus])
+*/
+
+const person ={name: "Amit"};
+
+console.log(person);
