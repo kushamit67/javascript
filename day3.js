5dbb9f5a6e1821e6532b513a3d1dc779019b85d8
kushamit67 <100716318+kushamit67@users.noreply.github.com>
2024-05-30 18:52:29 +0530

    Add files via upload
    30 May string, numbers done

---
+++ day3.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(null > 1);
+// console.log(undefined>=1)
+// console.log(2===1);
+
+let userOne ={
+    email: "ami@test.com",
+    pin: 110045
+}
+
+let userTwo = userOne
+
+userTwo.email ="amit@TextDecoderStream"
+
+console.log(userOne.email);
+console.log(userTwo.email);
---
+++ number.js
@@ -0,0 +1,23 @@
+const numOne = 300
+
+console.log(numOne);
+
+const numTwo = new Number (350.5999)
+console.log(numTwo.toString.length);
+console.log(numTwo.toFixed(2));
+console.log(numTwo.toPrecision(4));
+
+const bigNumber = 1894780038
+
+console.log(bigNumber.toLocaleString('en-IN'));
+
+console.log(Math.random())
+console.log((Math.random()*10 + 1))
+console.log(Math.floor(Math.random()*10 + 1))
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max - min)) + min)
+
+console.log(Math.floor(Math.random() * 6 ) + 1)
