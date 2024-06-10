230a5e1013b4e8fa6360df1b6317c90309c53318
Amit kush <creativeimaginator01@gmail.com>
2024-06-10 15:30:54 +0530

    Initial Commit

---
+++ string.js
@@ -0,0 +1,25 @@
+const name = "Amit Kush"
+const countRepo = 30
+
+console.log(`hello , My name is ${name} and I have ${countRepo} on github`)
+
+const gameName = new String('AmitGamer');
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('G'));
+console.log(gameName.slice(0, 5))
+console.log(gameName.substring(0, 3))
+
+const newString = "  Amit "
+console.log(newString);
+console.log(newString.trim())
+
+const urlOne = "https://creativeimaginator.com/page%39one"
+console.log(urlOne.replace('%39', '-'))
+
+const secondString = new String ('Amit kush cool')
+console.log(secondString)
+console.log(secondString.split(' '))
